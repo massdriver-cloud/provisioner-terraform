e9eccd8a9eee444ddca4c47a30badc7d11a1cef0
Christopher Hill <chris@massdriver.cloud>
2024-11-11 12:54:53 -0700

    initial working version (#1)
    * initial working version
    
    * copy updates from opentofu
    
    * remove Makefile
    
    * README update
    
    * README update

--- Dockerfile
+++ Dockerfile
@@ -1,50 +1,52 @@
 # This Dockerfile is intended to be built from the top of the repo (not this directory)
 ARG XO_VERSION=latest
-ARG TERRAFORM_VERSION=1.3.6
-ARG RUN_IMG=debian:11.3-slim
-ARG TF_CACHE_DIR=/tfcache
+ARG TERRAFORM_VERSION=1.5.7
+ARG CHECKOV_VERSION=3.2.268
+ARG OPA_VERSION=0.69.0
+ARG RUN_IMG=debian:12.7-slim
 ARG USER=massdriver
 ARG UID=10001
 
-# FROM 005022811284.dkr.ecr.us-west-2.amazonaws.com/massdriver-cloud/xo:${XO_VERSION} as xo
+FROM 005022811284.dkr.ecr.us-west-2.amazonaws.com/massdriver-cloud/xo:${XO_VERSION} AS xo
 
-FROM ${RUN_IMG} as bundles
+FROM ${RUN_IMG} AS build
 ARG TERRAFORM_VERSION
-ARG TF_CACHE_DIR
+ARG CHECKOV_VERSION
+ARG OPA_VERSION
 
 # install terraform, opa, yq, massdriver cli, kubectl
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt update && apt install -y curl unzip make jq && \
     rm -rf /var/lib/apt/lists/* && \
     curl -sSL https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip > terraform.zip && unzip -d /usr/local/bin/ terraform.zip && rm *.zip && \
-    curl -sSL https://openpolicyagent.org/downloads/v0.41.0/opa_linux_amd64_static > /usr/local/bin/opa && chmod a+x /usr/local/bin/opa && \
-    curl -sSL https://github.com/bridgecrewio/checkov/releases/download/3.2.38/checkov_linux_X86_64_3.2.38.zip > checkov.zip && unzip -d /usr/local/bin/ checkov.zip && rm *.zip
+    curl -sSL https://openpolicyagent.org/downloads/v${OPA_VERSION}/opa_linux_amd64_static > /usr/local/bin/opa && chmod a+x /usr/local/bin/opa && \
+    curl -sSL https://github.com/bridgecrewio/checkov/releases/download/${CHECKOV_VERSION}/checkov_linux_X86_64.zip > checkov.zip && unzip checkov.zip && mv dist/checkov /usr/local/bin/ && rm *.zip
+
+COPY --from=xo /usr/bin/xo /usr/local/bin/xo
 
 FROM ${RUN_IMG}
-ARG TF_CACHE_DIR
 ARG USER
 ARG UID
 
-RUN apt update && apt install -y git jq vim-tiny && \
+RUN apt update && apt install -y ca-certificates jq git && \
     rm -rf /var/lib/apt/lists/*
 
-RUN mkdir -m 777 /out
-RUN mkdir -m 777 /scratch
-RUN mkdir -p -m 777 /src/bundles
+RUN mkdir -p -m 777 /massdriver
 
 RUN adduser \
     --disabled-password \
     --gecos "" \
     --uid $UID \
     $USER
-RUN chown -R $USER:$USER /src
+RUN chown -R $USER:$USER /massdriver
 USER $USER
 
-COPY --from=bundles /usr/local/bin/* /usr/local/bin/
+COPY --from=build /usr/local/bin/* /usr/local/bin/
+COPY ./opa /opa
 COPY entrypoint.sh /usr/local/bin/
 
 ENV MASSDRIVER_PROVISIONER=terraform
 
-WORKDIR /src/bundles
+WORKDIR /massdriver
 
 ENTRYPOINT ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,73 @@
 #!/bin/bash
+set -euo pipefail
 
-set -eo pipefail
+entrypoint_dir="/massdriver"
 
-case $PROVISIONER_ACTION in
-    apply )
+params_path="$entrypoint_dir/params.json"
+connections_path="$entrypoint_dir/connections.json"
+config_path="$entrypoint_dir/config.json"
+envs_path="$entrypoint_dir/envs.json"
+secrets_path="$entrypoint_dir/secrets.json"
+
+# Extract provisioner configuration
+json_output=$(jq -r '.json // false' "$config_path")
+
+# Extract Checkov configuration
+checkov_enabled=$(jq -r '.checkov.enable // true' "$config_path")
+checkov_quiet=$(jq -r '.checkov.quiet // true' "$config_path")
+checkov_halt_on_failure=$(jq -r '.checkov.halt_on_failure // false' "$config_path")
+
+function evaluate_checkov() {
+    if [ "$checkov_enabled" = "true" ]; then
+        echo "evaluating Checkov policies"
+        checkov_flags=""
+
+        if [ "$checkov_quiet" = "true" ]; then
+            checkov_flags+=" --quiet"
+        fi
+        if [ "$checkov_halt_on_failure" = "false" ]; then
+            checkov_flags+=" --soft-fail"
+        fi
+
+        checkov --framework terraform_plan -f tfplan.json $checkov_flags --download-external-modules false --repo-root-for-plan-enrichment . --deep-analysis
+    fi
+}
+
+# Setup envs for Massdriver HTTP state backend 
+MASSDRIVER_SHORT_PACKAGE_NAME=$(echo $MASSDRIVER_PACKAGE_NAME | sed 's/-[a-z0-9]\{4\}$//')
+export TF_HTTP_USERNAME=${MASSDRIVER_DEPLOYMENT_ID}
+export TF_HTTP_PASSWORD=${MASSDRIVER_TOKEN}
+export TF_HTTP_ADDRESS="https://api.massdriver.cloud/state/${MASSDRIVER_SHORT_PACKAGE_NAME}/${MASSDRIVER_STEP_PATH}"
+export TF_HTTP_LOCK_ADDRESS=${TF_HTTP_ADDRESS}
+export TF_HTTP_UNLOCK_ADDRESS=${TF_HTTP_ADDRESS}
+
+# Have to copy the secrets file to the bundle directory for backwards compatibility with the legacy provisioner.
+# This has been deprecated and should be removed in the future once users have had a chance to update their bundles.
+if [ -f "$secrets_path" ]; then
+    cp "$secrets_path" "$entrypoint_dir/bundle/secrets.json"
+fi
+
+cd bundle/$MASSDRIVER_STEP_PATH
+
+# Copy the params/connections files to the step directory
+cp "$connections_path" _connections.auto.tfvars.json
+cp "$params_path" _params.auto.tfvars.json
+
+tf_flags="-input=false"
+if [ "$json_output" = "true" ]; then
+    tf_flags+=" -json"
+fi
+
+case $MASSDRIVER_DEPLOYMENT_ACTION in
+    plan )
+        command=plan
+        ;;
+    provision )
         command=apply
         ;;
-    destroy )
+    decommission )
         command=destroy
+        tf_flags+=" -destroy"
         ;;
     *)
         echo "Unsupported action: $action"
@@ -15,21 +75,49 @@ case $PROVISIONER_ACTION in
         ;;
 esac
 
-echo "copying variables files"
-cp /src/bundles/connections.json _connections.auto.tfvars.json
-cp /src/bundles/params.json _params.auto.tfvars.json
+xo provisioner terraform backend http -s "$MASSDRIVER_STEP_PATH" -o backend.tf.json
 
-tf_flags=""
-if [ $command = "destroy" ]; then
-    tf_flags="-destroy"
+terraform init -input=false
+terraform plan $tf_flags -out tf.plan
+
+# Run validations if the command is not 'destroy'
+if [ "$MASSDRIVER_DEPLOYMENT_ACTION" != "decommission" ]; then
+    terraform show -json tf.plan > tfplan.json
+
+    # Run Checkov if enabled
+    evaluate_checkov
+
+    # Check for invalid deletions
+    if [ -f validations.json ]; then
+        echo "evaluating OPA rules"
+        opa eval --fail-defined --data /opa/terraform.rego --input tfplan.json --data validations.json "data.terraform.deletion_violations[x]" > /dev/null
+    fi
 fi
 
-echo "executing terraform init"
-terraform init -no-color -input=false
-echo "executing terraform plan"
-terraform plan $tf_flags -out tf.plan -json
-echo "executing terraform apply"
-terraform apply $tf_flags -json tf.plan
+if [ "$MASSDRIVER_DEPLOYMENT_ACTION" = "plan" ]; then
+    exit 0
+fi
 
-rm _connections.auto.tfvars.json
-rm _params.auto.tfvars.json
+terraform apply $tf_flags tf.plan
+
+# Handle artifacts if deployment action is 'provision' or 'decommission'
+case "$MASSDRIVER_DEPLOYMENT_ACTION" in
+    provision )
+        terraform show -json  | jq '.values.outputs // {}' > outputs.json
+        jq -s '{params:.[0],connections:.[1],outputs:.[2]}' "$params_path" "$connections_path" outputs.json > artifact_inputs.json
+        for artifact_file in artifact_*.jq; do
+            [ -f "$artifact_file" ] || break
+            field=$(echo "$artifact_file" | sed 's/^artifact_\(.*\).jq$/\1/')
+            echo "Creating artifact for field $field"
+            jq -f "$artifact_file" artifact_inputs.json | xo artifact publish -d "$field" -n "Artifact $field for $name_prefix" -f -
+        done
+        ;;
+    decommission )
+        for artifact_file in artifact_*.jq; do
+            [ -f "$artifact_file" ] || break
+            field=$(echo "$artifact_file" | sed 's/^artifact_\(.*\).jq$/\1/')
+            echo "Deleting artifact for field $field"
+            xo artifact delete -d "$field" -n "Artifact $field for $name_prefix"
+        done
+        ;;
+esac
